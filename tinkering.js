b4f4e86a5888065b7e3b5b462389d18a7cadaa5b
Edward Smith <edward.m.smith80@gmail.com>
2020-07-26 18:48:02 +0000

    this is a commit message for tinkering.js written in nano.. this is a result of typing 'git commit tinkering.js' and then nano pops up... and then i typed this

--- tinkering.js
+++ tinkering.js
@@ -1,7 +1,45 @@
-function multiply(a, b) {
-  return a * b;
+const amounts = [61.00, 52.25, 112.99,  ,5.00];
+
+let total = 0;
+for (let i = 0; i < amounts.length; i++) {
+  total += amounts[i];
 }
 
-const result = multiply(2, 4);
+console.log('Order total is: ', total);
 
-console.log(result);
+// here's the same thing as above but with a for..of loop
+
+for (let amount of amounts) {
+  console.log(amount);
+  total += amount;
+}
+console.log('Order total is: ', total); 
+
+
+amounts.forEach(num => {
+  
+  total += num;});
+
+//functions exercises
+console.log("CHAPTER 3 UDACITY: FUNCTIONS");
+
+function findAverage(x,y) {
+  var avg = (x + y) / 2;
+  return avg;
+}
+
+var avg = findAverage(5, 9);
+console.log(avg);
+
+console.log("SHADOWING BUG EXAMPLE!!!*****************");
+
+let bookTitle = "Le Petit Prince";
+console.log(bookTitle);
+
+function displayBookEnglish() {
+  let  bookTitle = "The Little Prince";
+  console.log(bookTitle);
+}
+
+displayBookEnglish();
+console.log(bookTitle);
